d74b2a1cc5b5710d7074eced678e755a7fc9bf1a
Thomkam <thomas-kamoun@hotmail.fr>
2024-03-20 15:54:04 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-/* import { useState } from 'react'
- */ import "./App.css";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return <></>;
+  const [btnOn, setBtnOn] = useState(true);
+
+  return (
+    <>
+      <div className="container">
+        <button
+          style={{ backgroundColor: btnOn ? "green" : "blue" }}
+          onClick={() => {
+            setBtnOn(btnOn);
+          }}
+        >
+          ON
+        </button>
+        <button
+          style={{ backgroundColor: !btnOn ? "blueviolet" : "white" }}
+          onClick={() => {
+            setBtnOn(!btnOn);
+          }}
+        >
+          OFF
+        </button>
+      </div>
+
+      <div>
+        <button /* style = {{ btnOn === true ? "green" : "red"}} */>
+          GO !
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
